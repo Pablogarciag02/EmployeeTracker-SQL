4343f521105e97c9abccd05c161b1d6c46862cf4
Pablo Garcia <pgarciag566@gmail.com>
2022-05-30 18:12:57 -0500

    contains specific sql queries like the example vid

---
+++ db/queries.js
@@ -0,0 +1,20 @@
+const queries = {
+    allEmployees:`SELECT e.id, e.first_name, e.last_name, title, name AS department, salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    INNER JOIN role
+    ON e.role_id = role.id
+    INNER JOIN department
+    ON role.department_id = department.id
+    LEFT JOIN employee m
+    ON e.manager_id = m.id;`,
+
+    allRoles:`SELECT role.id, title, salary, name AS department
+    FROM role
+    INNER JOIN department
+    ON role.department_id = department.id;`,
+
+    allDepartments:`SELECT id, name AS department
+    FROM department;`
+}
+
+module.exports = queries;
