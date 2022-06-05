0df609135b02e8c694b79e0cb1a43128c76e6534
Pablo Garcia <pgarciag566@gmail.com>
2022-06-05 00:02:45 -0500

    added cascading when deleting a primary key

--- db/employeetracker_db.sql
+++ db/employeetracker_db.sql
@@ -15,7 +15,7 @@ title VARCHAR (30),
 salary DECIMAL,
 department_id INTEGER ,
 PRIMARY KEY (id),
-FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE
+FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
@@ -25,6 +25,6 @@ last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
 PRIMARY KEY (id),
-FOREIGN KEY (manager_id) REFERENCES role(id) ON UPDATE CASCADE
+FOREIGN KEY (manager_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
